4164c303ad58357e7b4c7f14662eb7d6df4118d2
Joe Previte <jjprevite@gmail.com>
2023-08-30 18:38:19 +0000

    update

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -1,5 +1,91 @@
-describe("test", () => {
-  it("should pass", () => {
-    expect(1).toBe(1);
+import { parseCsv, parseCsvLine } from "../src/index";
+
+describe("parseCsv", () => {
+  it.skip("should parse and transform CSV data correctly with 1 row", () => {
+    const input = `book_id,name,description,price
+1,Hello,good,20`;
+
+    const expectedOutput = [
+      {
+        book_id: "1",
+        name: "Hello",
+        description: "good",
+        price: "20",
+      },
+    ];
+
+    const output = parseCsv(input);
+
+    expect(output).toEqual(expectedOutput);
+  });
+  it("should parse and transform CSV data correctly", () => {
+    const input = `book_id,name,description,price
+1,Hello,good,20
+2,Harry Potter,a popular book,$30
+3,Book, too much      whitespace    , $10
+4d,the dictionary,"  a description, including a comma  ", $30.30`;
+
+    const expectedOutput = [
+      {
+        book_id: "1",
+        name: "Hello",
+        description: "good",
+        price: "20",
+      },
+      {
+        book_id: "2",
+        name: "Harry Potter",
+        description: "a popular book",
+        price: "$30",
+      },
+      {
+        book_id: "3",
+        name: "Book",
+        description: "too much      whitespace",
+        price: "$10",
+      },
+      {
+        book_id: "4d",
+        name: "the dictionary",
+        description: '"  a description, including a comma  "',
+        price: "$30.30",
+      },
+    ];
+
+    const output = parseCsv(input);
+
+    expect(output).toEqual(expectedOutput);
+  });
+  it("should parse and transform CSV data correctly (edge case 4)", () => {
+    const input = `book_id,name,description,price
+4d,the dictionary,"  a description, including a comma  ", $30.30`;
+
+    const expectedOutput = [
+      {
+        book_id: "4d",
+        name: "the dictionary",
+        description: '"  a description, including a comma  "',
+        price: "$30.30",
+      },
+    ];
+
+    const output = parseCsv(input);
+
+    expect(output[0].description).toEqual(expectedOutput[0].description);
+  });
+});
+
+describe.only("parseCsvLine", () => {
+  it.skip("should handle the happy path", () => {
+    const input = " $30.30";
+    const output = parseCsvLine(input);
+    const expected = ["$30.30"];
+    expect(output).toEqual(expected);
+  });
+  it("should handle double quotes", () => {
+    const input = '"  a description, including a comma  "';
+    const output = parseCsvLine(input);
+    const expected = ['"  a description, including a comma  "'];
+    expect(output).toEqual(expected);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,50 @@
 // add your code here 🎉
+
+type FixMeLater = any;
+
+export function parseCsv(csvData: string): Record<string, string>[] {
+  const lines = csvData.trim().split("\n");
+  if (lines.length === 0) {
+    return [];
+  }
+
+  const headers = lines[0].split(",").map((header) => header.trim()); // Trim headers
+  const parsedData: Record<string, string>[] = [];
+
+  for (let i = 1; i < lines.length; i++) {
+    const values = parseCsvLine(lines[i]);
+    if (values.length === headers.length) {
+      const entry: Record<string, string> = {};
+      for (let j = 0; j < headers.length; j++) {
+        entry[headers[j]] = values[j];
+      }
+      parsedData.push(entry);
+    }
+  }
+
+  return parsedData;
+}
+
+// "  a description, including a comma  "
+export function parseCsvLine(line: string): string[] {
+  const values = [];
+  let current = "";
+  let insideQuotes = false;
+
+  for (let i = 0; i < line.length; i++) {
+    const char = line[i];
+
+    if (char === '"') {
+      //   insideQuotes = !insideQuotes;
+      values.push(char);
+    } else if (char === "," && !insideQuotes) {
+      values.push(current.trim());
+      current = "";
+    } else {
+      current += char;
+    }
+  }
+
+  values.push(insideQuotes ? current : current.trim());
+  return values.map((value) => value.replace(/^"|"$/g, "")); // Remove surrounding double quotes
+}
